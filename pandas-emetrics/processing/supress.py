b3f50d5eeeb26d13f7033d6d101856e95d9d3703
Noah Kasica <nkasica21@gmail.com>
2024-12-05 18:56:59 -0500

    added processing folder and supress.py

---
+++ pandas-emetrics/processing/supress.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+# register function as pandas dataframe accessor
+@pd.api.extensions.register_dataframe_accessor("supress")
+class SupressAcessor:
+    def __init__(self, pandas_obj):
+        self._obj = pandas_obj
+
+    def __call__(self, columns: list[str], supressor: str) -> pd.DataFrame:
+        """
+        Returns an updated DataFrame with entries in the given columns supressed.
+        
+        Parameters
+        ----------
+        columns: list[str]
+            List of DataFrame's columns to supress
+            Example: columns=['Name', 'ID']
+
+        supressor: str
+            A string or symbol used to replaces the column's entries
+            Example: supressor='*'
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame with supressed columns
+        """
+
+        # replaces all values in given columns with the supressor
+        for column in columns:
+            self._obj[column] = supressor
+
+        return self._obj
