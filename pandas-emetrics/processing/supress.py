e36fa49bd540b09f260d975cc1b0f33022b5d65e
Noah Kasica <nkasica21@gmail.com>
2024-12-06 21:15:03 -0500

    added inplace parameter to suppress.py

--- pandas-emetrics/processing/supress.py
+++ pandas-emetrics/processing/supress.py
@@ -6,7 +6,7 @@ class SupressAccessor:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
-    def __call__(self, columns: list[str], supressor: str) -> pd.DataFrame:
+    def __call__(self, columns: list[str], supressor: str, inplace: bool=False) -> pd.DataFrame:
         """
         Returns an updated DataFrame with entries in the given columns supressed
         
@@ -20,14 +20,29 @@ class SupressAccessor:
             A string or symbol used to replaces the column's entries
             Example: supressor='*'
 
+        inplace: bool
+            Optional parameter. If True, does not modify the dataframe and returns a copy instead
+            Example: inplace=True
+
         Returns
         -------
         pd.DataFrame
             DataFrame with supressed columns
         """
 
-        # replaces all values in given columns with the supressor
-        for column in columns:
-            self._obj[column] = supressor
+        # in-place modification
+        if inplace:
+            # replaces all values in given columns with the supressor
+            for column in columns:
+                self._obj[column] = supressor
 
-        return self._obj
+            return self._obj
+
+        else:
+            # create copy of dataframe
+            df = self._obj.copy(deep=True)
+
+            for column in columns:
+                df[column] = supressor
+
+            return df
