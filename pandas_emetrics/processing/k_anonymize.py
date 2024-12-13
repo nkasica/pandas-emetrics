e51b53bcb1fe6515436d04e5f6198c3d7bb7aebe
Noah Kasica <nkasica21@gmail.com>
2024-12-13 15:17:02 -0500

    k_anonymize functiongit add .!

--- pandas_emetrics/processing/k_anonymize.py
+++ pandas_emetrics/processing/k_anonymize.py
@@ -6,9 +6,43 @@ class KAnonymizeAccessor:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
+    def summary(partition: pd.DataFrame, quasi: list[str]) -> pd.DataFrame:
+        """
+        Generalize each partition for each quasi identifier based on min-max range.
+        """
+        for id in quasi:
+            partition = partition.sort_values(by=id)
+            s = f'[{partition[id].iloc[0]}-{partition[id].iloc[-1]}]'
+            partition[id] = [s] * partition[id].size
+
+        return partition
+
+    def anonymize(partition, quasi, frequency_set, k):
+        """
+        Recursively partitions the quasi identifiers
+        """
+
+        # sorts DataFrame the quasi identifier with the most unique values
+        qi = frequency_set[0][0]
+        partition = partition.sort_values(by=qi)
+
+        # find median idx to split on
+        splitVal = partition[qi].count() // 2
+        lhs = partition[splitVal:]
+        rhs = partition[:splitVal]
+
+        # recursively anonymize
+        if (len(lhs) >= k and len(rhs) >= k):
+            return pd.concat([KAnonymizeAccessor.anonymize(lhs, quasi, frequency_set, k), 
+                             KAnonymizeAccessor.anonymize(rhs, quasi, frequency_set, k)])
+
+        # return partitioned grouping to be generalized
+        return KAnonymizeAccessor.summary(partition, quasi)
+
     def __call__(self, quasi: list[str], k: int) -> pd.DataFrame:
         """
         Returns DataFrame, if possible, after being k-anonymized with the given k value.
+        This operation is not in place and only works for numeric data point.
 
         Parameters
         ----------
@@ -25,4 +59,15 @@ class KAnonymizeAccessor:
             K-Anonymized DataFrame, if possible
         """ 
 
-        
+        if k > self._obj.shape[0]:
+            raise ValueError("K must be less than or equal to the number of samples in the DataFrame.")
+
+        # get value counts for each quasi identifier
+        frequency_set = {}
+        for id in quasi:
+            frequency_set[id] = self._obj[id].nunique()
+
+        # sort by value descending
+        frequency_set = sorted(frequency_set.items(), key=lambda x: x[1], reverse=True)
+
+        return KAnonymizeAccessor.anonymize(self._obj, quasi, frequency_set, k).sort_index()
