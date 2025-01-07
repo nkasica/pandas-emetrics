d4086aa05bd496f13cb2374a8bafe794f87fd601
Noah Kasica <nkasica21@gmail.com>
2025-01-06 21:55:14 -0500

    docstring + comment changes

--- pandas_emetrics/processing/suppress.py
+++ pandas_emetrics/processing/suppress.py
@@ -3,49 +3,47 @@ import pandas as pd
 # register function as pandas dataframe accessor
 @pd.api.extensions.register_dataframe_accessor("suppress")
 class SuppressAccessor:
+
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
     def __call__(self, columns: list[str], suppressor: str, inplace: bool=False) -> None | pd.DataFrame:
         """
-        Returns an updated DataFrame with entries in the given columns suppressed
+        Replaces columns in the DataFrame with the given suppressor.
         
         Parameters
         ----------
         columns: list[str]
-            List of DataFrame's columns to suppress
+            List of DataFrame's columns to suppress.
             Example: columns=['Name', 'ID']
 
         supressor: str
-            A string or symbol used to replaces the column's entries
+            A string used to replaces the column's entries
             Example: supressor='*'
 
         inplace: bool
-            Optional parameter. If True, does not modify the dataframe and returns a copy instead
+            Determines whether or not to override current DataFrame values.
+            Defaults to False.
             Example: inplace=True
 
         Returns
         -------
         None | pd.DataFrame
-            Returns None if 'inplace=True'. Otherwise, returns DataFrame with suppressed columns
+            Returns None if 'inplace=True'. Otherwise, returns DataFrame with suppressed columns.
         """
 
+        # prevents future assignment errors
         if not isinstance(suppressor, str):
-            raise ValueError("Suppressor must be a string")
+            raise ValueError("Suppressor must be a string.")
 
-        # in-place modification
         if inplace:
-            # replaces all values in given columns with the supressor
+            # replaces all values in given columns with the suppressor
             for column in columns:
                 self._obj[column] = suppressor
-
             return None
-
         else:
             # create copy of dataframe
             df = self._obj.copy(deep=True)
-
             for column in columns:
                 df[column] = suppressor
-
             return df
