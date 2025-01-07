7f6c58da52a32285d10c2a9661d42931166a640e
Noah Kasica <nkasica21@gmail.com>
2025-01-06 21:58:50 -0500

    formatting

--- pandas_emetrics/metrics/l_diversity.py
+++ pandas_emetrics/metrics/l_diversity.py
@@ -2,26 +2,28 @@ import pandas as pd
 
 @pd.api.extensions.register_dataframe_accessor("l_diversity")
 class LDiversityAccessor:
+
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
     def __call__(self, quasi: list[str], sensitive: list[str]) -> int:
         """
-        Returns l-diversity value of the dataset
+        Returns l-diversity value of the DataFrame.
 
         Parameters
         ----------
         quasi: list[str]
             List of DataFrame's quasi identifiers
             Example: quasi=['Age', 'Height', 'Weight']
-        sensitive
+
+        sensitive: list[str]
             List of DataFrame's sensitive attribute(s)
             Example: sensitive=['Salary']
 
         Returns
         -------
         int 
-            The calculated l-value
+            The l-value of the DataFrame.
         """
         
         # get equivalence classes
@@ -31,11 +33,9 @@ class LDiversityAccessor:
         min_l = len(self._obj)
 
         for _, group in equivalence_classes:
-
             for attr in sensitive:
                 # num of unique equivalence class combinations 
                 l_eq = group[attr].drop_duplicates().shape[0]
-
                 min_l = min(l_eq, min_l)
 
         return min_l
